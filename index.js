f5303f5ffed630f7cd1deb01919723c4dfe54fcc
crescentpartha <crescentpartha406@gmail.com>
2023-01-12 16:48:24 +0600

    get all comments data from database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,31 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// connection setup with database with secure password on environment variable
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fhgmvqu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// Create dynamic data and send to the database
+async function run() {
+    try {
+        await client.connect();
+        console.log('banglaEdTechResources database connected');
+        const commentCollection = client.db('banglaEdTechResources').collection('comment');
+
+        // get all comments data from database
+        app.get('/comment', async (req, res) => {
+            const query = {};
+            const cursor = commentCollection.find(query);
+            const comments = await cursor.toArray();
+            res.send(comments);
+        });
+    }
+    finally {
+        // await client.close(); // commented, if I want to keep connection active;
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running Bangla-EdTech-Resources Server');
---
+++ steps.md
@@ -0,0 +1,12 @@
+## Table of Contents
+
+- [Environment Variables](#environment-variables)
+
+# Environment Variables
+
+``` JavaScript
+DB_USER=dbUser
+DB_PASS=2o4pxnhgxpBBEZgG
+```
+
+**[🔼Back to Top](#table-of-contents)**
